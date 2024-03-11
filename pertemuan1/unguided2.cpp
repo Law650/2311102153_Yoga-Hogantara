e600c27126cb34cbe4da2819292f80ed2deb9639
Law650 <yogatara40@gmail.com>
2024-03-11 19:57:55 +0700

    Output

--- pertemuan1/unguided2.cpp
+++ pertemuan1/unguided2.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //program struct dan class
 #include  <iostream>
 
@@ -30,37 +29,4 @@ int main(){
     cout<<"MEREK SENAR : "<<steel.msenar<<endl;
     cout<<"UKURAN SENAR : "<<steel.ukuran<<endl;
     return 0;
-=======
-//program struct dan class
-#include  <iostream>
-
-using namespace std;
-
-class gitar{//class gitar 
-    public:
-    string merek;
-    string series;
-    float harga;
-};
-
-struct senar{//struct senar
-    string msenar;
-    int ukuran;
-};
-
-int main(){
-
-    cout<<"--CLASS--\n"; //deklarasi class yang menampilkan merek dan series dari gitar
-    gitar guitar;
-    guitar.merek ="Cort";
-    guitar.series ="ad810op";
-    cout<<"MEREK GITAR : "<<guitar.merek<<endl;
-    cout<<"SERIES GITAR: "<<guitar.series<<endl;
-
-    cout<<"++STRUCT++\n";//deklarasi struct yang menampilkan senar dan ukuran senar
-    senar steel{"Elixir",10};
-    cout<<"MEREK SENAR : "<<steel.msenar<<endl;
-    cout<<"UKURAN SENAR : "<<steel.ukuran<<endl;
-    return 0;
->>>>>>> 8cdf8a82430e4374359c0ac436634c2dcb89ba83
 }
--- pertemuan1/unguided3.cpp
+++ pertemuan1/unguided3.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <map>//agar map dapat berjalan
 
@@ -17,24 +16,4 @@ int main(){
         cout<<y<<"."<<gitar[y]<<endl;
     }
 return 0;
-=======
-#include <iostream>
-#include <map>//agar map dapat berjalan
-
-using namespace std;
-
-int main(){
-    //map. int untuk kunci dan string untuk merek gitar
-    map <int,string> gitar;
-    gitar[1]="Yamaha";
-    gitar[2]="Fender";
-    gitar[3]="Squire";
-    gitar[4]="Gibson";
-
-    //for untuk menampilkan nomor dan kunci dari map
-    for(int y = 1;y<=gitar.size();y++){
-        cout<<y<<"."<<gitar[y]<<endl;
-    }
-return 0;
->>>>>>> 8cdf8a82430e4374359c0ac436634c2dcb89ba83
 }
