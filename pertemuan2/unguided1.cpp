175b5fb9c6aadc86af467a28459590fcbc9cf6aa
Law650 <yogatara40@gmail.com>
2024-03-19 22:00:38 +0700

    LAPRAK2

--- pertemuan2/unguided1.cpp
+++ pertemuan2/unguided1.cpp
@@ -6,29 +6,29 @@ using namespace std;
 int main()
 {
     int bil_153[10];
-   // int size_153
-    cout<<"INPUT DATA ARRAY \n";
+    
+    cout<<"INPUT DATA ARRAY \n";//MENGINPUT ARRAY
     for(int y=1;y<=10;y++){
         cout<<"DATA ARRAY  "<<y<<" : ";
         cin>>bil_153[y];
     }
     cout<<endl;
 
-    cout<<"DATA ARRAY : ";
-    for(int y=0;y<=10;y++){
+    cout<<"DATA ARRAY : ";//MENAMPILKAN ARRAY
+    for(int y=1;y<=10;y++){
         cout<<bil_153[y]<<" ";
     }
     cout<<endl;
-    cout<<"Bilangan Genap : ";
-    for(int y=0;y<=10;y++){
+    cout<<"Bilangan Genap : ";//MENCARI BILANGAN GENAP
+    for(int y=1;y<=10;y++){
         if(bil_153[y]%2==0){
             cout<<bil_153[y]<<",";
         }
     }
     cout<<endl;
 
-    cout<<"Bilangan Gajil : ";
-    for(int y=0;y<=10;y++){
+    cout<<"Bilangan Gajil : ";//MENCARI BILANGAN GANJIL
+    for(int y=1;y<=10;y++){
         if(bil_153[y]%2!=0){
             cout<<bil_153[y]<<",";
         }
